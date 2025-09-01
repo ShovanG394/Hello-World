5f3637730068f5776b87ed6179dc05e2f228f16d
ShovanG394 <goswamishovan394@gmail.com>
2025-09-01 15:49:59 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,25 @@
+# Define two matrices
+A = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+
+B = [
+    [7, 8, 9],
+    [10, 11, 12]
+]
+
+# Initialize result matrix with zeros
+result = [
+    [0, 0, 0],
+    [0, 0, 0]
+]
+
+# Add matrices
+for i in range(len(A)):
+    for j in range(len(A[0])):
+        result[i][j] = A[i][j] + B[i][j]
+
+print("Sum of matrices:")
+for row in result:
+    print(row)
